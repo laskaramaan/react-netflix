a32c1e8d01ec6aedb96e2c83378fe11238e14593
Amaan <laskaramaan@gmail.com>
2022-06-12 17:20:11 +0530

    Initial Commit

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = '6aea8ef8924b56f4f0a5a7214395d28f'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests
